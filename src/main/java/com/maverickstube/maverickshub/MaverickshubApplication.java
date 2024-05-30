aaeb1a8fb84b41560928cd3400e67a35b38ffaf3
susannaholubiyi <susannaholubiyi@gmail.com>
2024-05-30 17:06:41 +0100

    first commit

---
+++ src/main/java/com/maverickstube/maverickshub/MaverickshubApplication.java
@@ -0,0 +1,13 @@
+package com.maverickstube.maverickshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaverickshubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaverickshubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maverickstube/maverickshub/MaverickshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maverickstube.maverickshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaverickshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
